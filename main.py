7a9d9bfea08eb77d6ce9d287a69d96f0a392b2b8
Wei Xie <xiewei.fire@gmail.com>
2017-08-02 16:22:20 +0800

    better CSS and remove pause
    Signed-off-by: Wei Xie <xiewei.fire@gmail.com>

--- main.py
+++ main.py
@@ -8,70 +8,43 @@ import codecs
 
 html='''
 <!doctype html>
-<!--
-
-  Instructions:
-
-  - Save this file.
-  - Replace "USER" with your GitHub username.
-  - Replace "REPO" with your GitHub repo name.
-  - Replace "Your Project" with your project name.
-  - Upload this file (or commit to GitHub Pages).
-
-  Customize as you see fit!
-
--->
 <html>
 <head>
   <meta charset='utf-8'>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width">
-
-  <title>Your Project</title>
-
+  <title></title>
   <!-- Flatdoc -->
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src='https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/legacy.js'></script>
   <script src='https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/flatdoc.js'></script>
-
   <!-- Flatdoc theme -->
   <link  href='https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/theme-white/style.css' rel='stylesheet'>
   <script src='https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/theme-white/script.js'></script>
-
-  <!-- Meta -->
-  <meta content="Your Project" property="og:title">
-  <meta content="Your Project description goes here." name="description">
-
   <!-- Initializer -->
   <script>
-    Flatdoc.run({
-      fetcher: Flatdoc.file('%s')
-    });
+       Flatdoc.run({
+          fetcher: Flatdoc.file('%s')
+       });
   </script>
+  <style>
+    .content h1 {font-size: 2em;padding-top:60px;}
+    .content h2 {font-size: 1.5em;padding-top:50px;}
+    .content h3 {font-size: 1.4em;padding-top:50px !important;}
+    .content p {font-size: 16px;font-family: Georgia;}
+    .content li {font-size:16px;font-family: Georgia;}
+    .content ul > li:before {top:9.5px;left:-23px;width:7px;height:7px;}
+    .content ul {margin-left:25px;}
+    .content blockquote {border-left: 4px solid #dddddd;  padding: 0 15px; color: #777777; }
+  </style>
 </head>
-<body role='flatdoc' class='no-literate'>
-
-  <div class='header'>
-    <div class='left'>
-      <h1>Your Project</h1>
-      <ul>
-        <li><a href='https://github.com/USER/REPO'>View on GitHub</a></li>
-        <li><a href='https://github.com/USER/REPO/issues'>Issues</a></li>
-      </ul>
-    </div>
-    <div class='right'>
-      <!-- GitHub buttons: see http://ghbtns.com -->
-      <iframe src="http://ghbtns.com/github-btn.html?user=USER&amp;repo=REPO&amp;type=watch&amp;count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe>
-    </div>
-  </div>
-
+<body role='flatdoc' class='big-h3 no-literate large-brief'>
   <div class='content-root'>
     <div class='menubar'>
       <div class='menu section' role='flatdoc-menu'></div>
     </div>
     <div role='flatdoc-content' class='content'></div>
   </div>
-
 </body>
 </html>
 '''
@@ -99,5 +72,3 @@ f.write(html)
 f.close()
 
 webbrowser.open(remoteFile)
-
-os.system('pause')
